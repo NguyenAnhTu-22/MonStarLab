2bf8f4579b3a7abe49d74779909d85aa356fc1c2
NguyenAnhTu-22 <56270025+NguyenAnhTu-22@users.noreply.github.com>
2020-07-16 15:20:32 +0700

    Update Bai2.java
    update

--- MonStarLab/src/Tuan1/Test1/Bai2.java
+++ MonStarLab/src/Tuan1/Test1/Bai2.java
@@ -8,18 +8,13 @@ public class Bai2 {
 		int soDien = sc.nextInt();
 		if (soDien <= 25) {
 			System.out.println("So Tien:" + (soDien * 1000));
-		} else {
-			if (soDien <= 75) {
-				System.out.println("So Tien:" + (25 * 1000 + (soDien - 25) * 1250));
-			} else {
-				if (soDien <= 150) {
-					System.out.println("So Tien:" + (25 * 1000 + 50 * 1250 + (soDien - 75) * 1800));
-				} else {
-					if (soDien > 150) {
-						System.out.println("SoTien:" + (25 * 1000 + 50 * 1250 + 75 * 1250 + (soDien) * 2500));
-					}
-				}
-			}
+		} else if (soDien <= 75) {
+			System.out.println("So Tien:" + (25 * 1000 + (soDien - 25) * 1250));
+		} else if (soDien <= 150) {
+			System.out.println("So Tien:" + (25 * 1000 + 50 * 1250 + (soDien - 75) * 1800));
+		} else if (soDien > 150) {
+			System.out.println("SoTien:" + (25 * 1000 + 50 * 1250 + 75 * 1250 + (soDien) * 2500));
 		}
 	}
+
 }
