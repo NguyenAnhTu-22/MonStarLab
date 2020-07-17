51aefe6d3ff8c460d86aab312a6e2134c4196f5d
NguyenAnhTu-22 <56270025+NguyenAnhTu-22@users.noreply.github.com>
2020-07-17 09:03:15 +0700

    sửa 4b

--- MonStarLab/src/Tuan1/Test1/News.java
+++ MonStarLab/src/Tuan1/Test1/News.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class News implements INews {
-	int id;
-	static String title;
-	static String publishDate;
-	static String author;
-	static String content;
-	float averageRate;
-	int[] rateList;
+	private int id;
+	private String title;
+	private String publishDate;
+	private String author;
+	private String content;
+	private float averageRate;
+	private int[] rateList;
+	
 	public static News news;
 	public static Scanner sc;
-	public static ArrayList<News> array;
+	public static ArrayList<News> array = new ArrayList<>();
 
 	public News(String title, String publishDate, String author, String content) {
 		this.title = title;
@@ -58,7 +59,7 @@ public class News implements INews {
 		return author;
 	}
 
-	public void setAuthor(String autor) {
+	public void setAuthor(String author) {
 		this.author = author;
 	}
 
@@ -81,10 +82,10 @@ public class News implements INews {
 	}
 
 	public void calculate() {
-		this.averageRate = (rateList[0] + rateList[1] + rateList[2]) / 3;
+		this.averageRate = (this.rateList[0] + this.rateList[1] + this.rateList[2]) / 3;
 	}
 
-	public void Menu() {
+	public static void Menu() {
 		System.out.println("1-Insert news");
 		System.out.println("2-View list news");
 		System.out.println("3-Average rate");
@@ -92,8 +93,8 @@ public class News implements INews {
 
 	}
 
-	public void func1() {
-		sc.nextLine();
+	public static void func1() {
+		sc = new Scanner(System.in);
 		System.out.println("Nhap title:");
 		String title = sc.nextLine();
 		System.out.println("Nhap publishDate:");
@@ -108,40 +109,42 @@ public class News implements INews {
 		news.rateList[0] = sc.nextInt();
 		news.rateList[1] = sc.nextInt();
 		news.rateList[2] = sc.nextInt();
-		array = new ArrayList<News>();
 		array.add(news);
 	}
 
-	public void func2() {
-		array.get(0).display();
+	public static void func2() {
+		for (int i = 0; i < array.size(); i++) {
+			array.get(i).display();
+		}
 	}
 
-	public void func3() {
-		array.get(0).calculate();
-		array.get(0).display();
+	public static void func3() {
+		for (int i = 0; i < array.size(); i++) {
+			array.get(i).calculate();
+			array.get(i).display();
+		}
 	}
 
 	public static void main(String[] args) {
-		News ne = new News(title, publishDate, author, content);
-
 		int i = 1;
 		while (i > 0) {
-			ne.Menu();
+			Menu();
 			System.out.println("Nhap lua chon");
 			sc = new Scanner(System.in);
 			int index = sc.nextInt();
 			switch (index) {
 			case 1:
-				ne.func1();
+				func1();
 				break;
 			case 2:
-				ne.func2();
+				func2();
 				break;
 			case 3:
-				ne.func3();
+				func3();
 				break;
 			case 4:
 				i = i - 1;
+				System.out.println("Thoat chuong trinh");
 				break;
 			}
 		}
